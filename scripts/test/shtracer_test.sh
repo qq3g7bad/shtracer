e071724d272b8b9ec46d43cc7abb2ad7190edbe5
qq3g7bad <qq3g7bad@gmail.com>
2024-10-21 23:50:30 +0900

    feat(test): global constant inital value

--- scripts/test/shtracer_test.sh
+++ scripts/test/shtracer_test.sh
@@ -2,9 +2,16 @@
 
 . "../../shtracer"
 
-echo "$0"
-test_global_constant() {
-	assertEquals "NONE" "${NODATA_STRING}"
+##
+# @brief  Default global constant
+test_default_global_constant() {
+	assertEquals "${CONFIG_PATH}" ""
+	assertEquals "${SHTRACER_MODE}" "NORMAL"
+	assertEquals "${AFTER_TAG}" ""
+	assertEquals "${BEFORE_TAG}" ""
+	assertEquals "${OUTPUT_DIR}" "./output/"
+	assertEquals "${SHTRACER_SEPARATOR}" "<shtracer_separator>"
+	assertEquals "${NODATA_STRING}" "NONE"
 }
 
 . "./shunit2/shunit2"
